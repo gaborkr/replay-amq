c576a297da4cd62cfefd63e731c5527a4e3baa2e
Gabor Kulcsar <gabor.kulcsar@masternaut.com>
2017-05-23 16:47:47 +0100

    Added mongoDb query and AMQ sender

--- replay.go
+++ replay.go
@@ -23,6 +23,10 @@ var stop = make(chan bool)
 var mongoUri = flag.String("mongoUri", "localhost:27017", "MongoDB URI")
 var mongoDb = flag.String("mongoDb", "customers_shared", "Mongo database to persist messages to")
 var mongoCollection = flag.String("mongoCollection", "amq_cep_replay", "Mongo collection to persist messages to")
+var mongoQuery = flag.String("query", "{}", "Mongo query to filter messages")
+
+var recvFlag = flag.Bool("recv", false, "Read messages from AMQ")
+var sendFlag = flag.Bool("send", false, "Send messages to AMQ")
 
 // these are the default options that work with RabbitMQ
 var options []func(*stomp.Conn) error = []func(*stomp.Conn) error{
@@ -35,37 +39,71 @@ type FrameFromAmq struct {
     Frame interface{}
 }
 
+func help() {
+    fmt.Fprintf(os.Stderr, "Usage of %s\n", os.Args[0])
+    flag.PrintDefaults()
+    os.Exit(1)
+}
+
 func main() {
     flag.Parse()
     if *helpFlag {
-        fmt.Fprintf(os.Stderr, "Usage of %s\n", os.Args[0])
-        flag.PrintDefaults()
-        os.Exit(1)
+        help()
     }
 
-    subscribed := make(chan bool)
-    go recvMessages(subscribed)
-
+    if *recvFlag {
+        log.Print("Mode: receive")
+        recvMessages()
+    } else if *sendFlag {
+        log.Print("Mode: send")
+        sendMessages()
+   } else {
+        help()
+    }
     // wait until we know the receiver has subscribed
-//    <-subscribed
-
-//    go sendMessages()
-
-//    <-stop
-    <-stop
 }
 
 func sendMessages() {
-    defer func() {
-        stop <- true
-    }()
 
     conn, err := stomp.Dial("tcp", *serverAddr, options...)
     if err != nil {
-        println("cannot connect to server", err.Error())
+        log.Fatal("cannot connect: ", err)
         return
     }
 
+    c := mongoConnect()
+    log.Print(c)
+
+    result := FrameFromAmq{}
+    var query interface{}
+//"{\"frame.assetId\": \"554994\"}"
+    err = bson.UnmarshalJSON([]byte(*mongoQuery), &query)
+    if err != nil {
+        log.Fatal(err)
+    }
+ 
+
+    iter := c.Find(query).Sort("ordinal").Limit(*messageCount).Iter()
+    
+    for iter.Next(&result) {
+
+        json, err2 := bson.MarshalJSON(result.Frame)
+        if err2 != nil {
+            log.Fatal(err2)
+        }
+        log.Print("result: ", result.Frame)
+        err = conn.Send(*queueName, "text/plain",
+            []byte(string(json)))
+        if err != nil {
+            log.Fatal(err)
+        }
+ 
+    }
+    if err := iter.Close(); err != nil {
+        log.Fatal(err)
+    }
+
+/*
     for i := 1; i <= *messageCount; i++ {
         text := fmt.Sprintf("Message #%d", i)
         err = conn.Send(*queueName, "text/plain",
@@ -75,41 +113,28 @@ func sendMessages() {
             return
         }
     }
-    println("sender finished")
+*/
+    conn.Disconnect()
+    log.Print("sender finished")
 }
 
-func recvMessages(subscribed chan bool) {
-    defer func() {
-        stop <- true
-    }()
+func recvMessages() {
 
     conn, err := stomp.Dial("tcp", *serverAddr, options...)
 
     if err != nil {
-        println("cannot connect to server", err.Error())
+        log.Fatal("cannot connect: ", err)
         return
     }
 
     sub, err := conn.Subscribe(*queueName, stomp.AckAuto)
     if err != nil {
-        println("cannot subscribe to", *queueName, err.Error())
+        log.Fatal("cannot subscribe to", *queueName, err)
         return
     }
-    close(subscribed)
     //----
 
-    log.Print("Connecting to MongoDB at ", *mongoUri)
-
-        session, err := mgo.Dial(*mongoUri)
-        if err != nil {
-                panic(err)
-        }
-        defer session.Close()
-
-        // Optional. Switch the session to a monotonic behavior.
-        session.SetMode(mgo.Monotonic, true)
-
-        c := session.DB(*mongoDb).C(*mongoCollection)
+    c := mongoConnect()
 
     //----
     for i := 1; i <= *messageCount; i++ {
@@ -136,8 +161,23 @@ func recvMessages(subscribed chan bool) {
 
     }
 
-    
     log.Print("receiver finished")
 
 }
 
+func mongoConnect() *mgo.Collection {
+    log.Print("Connecting to MongoDB at ", *mongoUri)
+
+    session, err := mgo.Dial(*mongoUri)
+    if err != nil {
+        panic(err)
+    }
+//    defer session.Close()
+
+    // Optional. Switch the session to a monotonic behavior.
+//    session.SetMode(mgo.Monotonic, true)
+
+    return session.DB(*mongoDb).C(*mongoCollection)
+
+}
+ 
